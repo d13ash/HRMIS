5a41eacbedcb53b05e1456005c2615815ccfb8fd
Megha Dewangan <meghadewangan1102@gmail.com>
2025-05-14 12:03:20 +0530

    home Page in green theme

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,25 +1,44 @@
 import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable, map, shareReplay } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  isMobile: Observable<boolean>;
+  menuOpen = false;
 
-  toaboutus(){
-    document.getElementById("about-usss")?.scrollIntoView({behavior:'smooth'})
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.isMobile = this.breakpointObserver.observe([Breakpoints.Handset]).pipe(
+      map((result) => result.matches),
+      shareReplay()
+    );
   }
 
-  contactus(){
-    document.getElementById("contact-usss")?.scrollIntoView({behavior:'smooth'})
+  toaboutus() {
+    document
+      .getElementById('about-usss')
+      ?.scrollIntoView({ behavior: 'smooth' });
   }
 
-  service(){
-    document.getElementById("services-usss")?.scrollIntoView({behavior:'smooth'})
+  contactus() {
+    document
+      .getElementById('contact-usss')
+      ?.scrollIntoView({ behavior: 'smooth' });
   }
 
-  portfolio(){
-    document.getElementById("portfolio-usss")?.scrollIntoView({behavior:'smooth'})
+  service() {
+    document
+      .getElementById('services-usss')
+      ?.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  portfolio() {
+    document
+      .getElementById('portfolio-usss')
+      ?.scrollIntoView({ behavior: 'smooth' });
   }
 }
